72df4e872ac053a31f0f96b956c1c1b97c33f104
David Van Fleet <dvfleet413@gmail.com>
2020-01-25 05:42:58 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    words = self.split
+    last_word = words.last 
+    last_word.end_with?('.')
   end
 
   def question?
-
+    words = self.split
+    last_word = words.last 
+    last_word.end_with?('?')
   end
 
   def exclamation?
-
+    words = self.split
+    last_word = words.last 
+    last_word.end_with?('!')
   end
 
   def count_sentences
-
+    self.split(/[!?.] /).length
   end
 end
